228493a2dcfbb9e36a71caf54756d4dfd7f6320c
LinXJ1204 <39145342+LinXJ1204@users.noreply.github.com>
2023-03-01 03:13:22 +0800

    vote1 update

--- Vote.sol
+++ Vote.sol
@@ -4,9 +4,71 @@ pragma solidity >=0.6.0 <0.9.0;
 
 contract Vote{
     address _constractowner;
-    constructor(){
+    uint _voter_amount;
+    uint _voted;
+    bool _voting_status;
+    uint _timestamp;
+    uint _duration;
+    uint _count;
+    address [] _candidate;
+
+    constructor(uint voter_amount, uint duration){
         _constractowner = msg.sender;
+        _voter_amount = voter_amount;
+        _voted = 0;
+        _voting_status = false;
+        _duration = duration;
+        _count = 0;
+    }
+    mapping(uint=>address) voter_list;
+    mapping(address=>bytes32) voter_ballot;
+
+    event Voter_set(address voter, uint id);
+    event Voting_complete(address voter);
+    event Vote_start(uint start_time);
+    event Vote_end(uint end_time);
+
+    function set_voter_list(address voter) private {
+        voter_list[_count] = voter;
+        _count++;
+        emit Voter_set(voter, _count-1);
     }
 
-    
+    function voting(bytes32 ballot) public{
+        require(msg.sender==voter_list[_count], "Wrong key");
+        voter_ballot[msg.sender] = ballot;
+        _voted++;
+        emit Voting_complete(msg.sender);
+    }
+
+    function voting_start() public {
+        require(msg.sender==_constractowner, "No Permission");
+        _voting_status = true;
+        _timestamp = block.timestamp;
+        emit Vote_start(_timestamp);
+    }
+
+    function voting_end() public {
+        require(msg.sender==_constractowner, "No Permission");
+        require(_timestamp-block.timestamp>_duration);
+        _voting_status = false;
+        emit Vote_end(_timestamp);
+    }
+
+    function get_ballot(address voter) public view returns(bytes32){
+        require(msg.sender==_constractowner, "No Permission");
+        return voter_ballot[voter];
+    }
+
+    function set_candidates(address [] memory candidates) public {
+        _candidate = candidates;
+    }
+
+    function set_voters(address [] memory voters) public {
+        require(msg.sender==_constractowner, "No Permission");
+        for(uint i = 0; i < voters.length; i++){
+            set_voter_list(voters[i]);
+        }
+    }
+
 }
---
+++ Vote_1.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+contract Vote{
+
+    address _constractowner;
+
+    constructor(){
+        _constractowner = msg.sender;
+
+    }
+
+    event Voter_set(address voter, uint id);
+    event Voting_complete(address voter);
+    event Vote_start(uint start_time);
+    event Vote_end(uint end_time);
+
+    struct _Proposal{
+        uint _proposal_id;
+        uint _voting_status;
+        uint _timestamp;
+        uint _duration;
+        uint _count;
+        uint _voted;
+        uint _voter_amount;
+        address [] _candidate;
+        mapping(uint=>address) voter_list;
+        mapping(address=>bytes32) voter_ballot;
+        mapping(address => bool) hasVoted;
+    }
+
+    mapping(uint => _Proposal) private _proposal;
+    mapping(uint => bool) private _proposal_id_used;
+
+    function new_proposal(uint proposal_id) public {
+        require(msg.sender==_constractowner, "No Permission");
+        require(!_proposal_id_used[proposal_id], "ID been used");
+        _proposal[proposal_id]._proposal_id = proposal_id;
+        _proposal[proposal_id]._voting_status = 0;
+        _proposal_id_used[proposal_id] = true;
+    }
+
+    function set_duration(uint duration, uint proposal_id) public {
+        require(msg.sender==_constractowner, "No Permission");
+        require(_proposal[proposal_id]._voting_status<1);
+        _proposal[proposal_id]._duration = duration;
+    }
+
+    function set_voter_list(address voter, uint proposal_id) private {
+        uint count = _proposal[proposal_id]._count;
+        _proposal[proposal_id].voter_list[count] = voter;
+        _proposal[proposal_id]._count++;
+        emit Voter_set(voter, count);
+    }
+
+    function voting(bytes32 ballot, uint proposal_id, uint voter_id) public{
+        require(_proposal[proposal_id]._voting_status==1);
+        require(msg.sender==_proposal[proposal_id].voter_list[voter_id], "Wrong key");
+        _proposal[proposal_id].voter_ballot[msg.sender] = ballot;
+        _proposal[proposal_id]._voted++;
+        emit Voting_complete(msg.sender);
+    }
+
+    function voting_start(uint proposal_id) public {
+        require(_proposal[proposal_id]._voting_status<1);
+        require(msg.sender==_constractowner, "No Permission");
+        _proposal[proposal_id]._voting_status ++;
+        _proposal[proposal_id]._timestamp = block.timestamp;
+        emit Vote_start(_proposal[proposal_id]._timestamp);
+    }
+
+    function voting_end(uint proposal_id) public {
+        require(_proposal[proposal_id]._voting_status==1);
+        require(msg.sender==_constractowner, "No Permission");
+        require(_proposal[proposal_id]._timestamp-block.timestamp>_proposal[proposal_id]._duration);
+        _proposal[proposal_id]._voting_status++;
+        emit Vote_end(_proposal[proposal_id]._timestamp);
+    }
+
+    function get_ballot(address voter, uint proposal_id) public view returns(bytes32){
+        require(msg.sender==_constractowner, "No Permission");
+        return _proposal[proposal_id].voter_ballot[voter];
+    }
+
+    function set_candidates(address [] memory candidates, uint proposal_id) public {
+        _proposal[proposal_id]._candidate = candidates;
+    }
+
+
+
+    function set_voters(address [] memory voters, uint proposal_id) public {
+        require(msg.sender==_constractowner, "No Permission");
+        for(uint i = 0; i < voters.length; i++){
+            set_voter_list(voters[i], proposal_id);
+        }
+    }
+
+}
